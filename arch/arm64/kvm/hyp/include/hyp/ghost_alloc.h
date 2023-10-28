615cf0b232cc3d8413ac223811cf0946464ca754
dk505 <dk505@cam.ac.uk>
2023-10-28 02:09:41 +0100

    kconfig the static dynamic memory pool

--- arch/arm64/kvm/hyp/include/hyp/ghost_alloc.h
+++ arch/arm64/kvm/hyp/include/hyp/ghost_alloc.h
@@ -3,8 +3,7 @@
 
 #include <linux/types.h>
 
-// XXX Kconfig for MAX_ORDER.
-#define GHOST_ALLOC_MAX_ORDER 22 // 4 MB
+#define GHOST_ALLOC_MAX_ORDER CONFIG_NVHE_GHOST_MEM_LOG2_MB
 #define GHOST_ALLOC_MIN_ORDER 5  // 32 B
 
 void *g_malloc(size_t size);
