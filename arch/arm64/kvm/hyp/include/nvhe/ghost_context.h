3900898fa002994058adef7abe933a9c94f317f5
bjs <benjsimner@gmail.com>
2023-11-15 11:10:06 +0000

    (ghost/context) add kernel-doc to explain macros

--- arch/arm64/kvm/hyp/include/nvhe/ghost_context.h
+++ arch/arm64/kvm/hyp/include/nvhe/ghost_context.h
@@ -9,6 +9,8 @@
  * and can be dumped at any time.
  */
 
+// TODO: make this use the ghost_printf machinery.
+
 typedef void (*ghost_printer_fn)(void *data);
 
 enum ghost_log_level {
@@ -39,11 +41,33 @@ int gp_put_current_context_trace(gp_stream_t *out);
 
 
 #define __INNER_NAME(name) "inner " name
+
+/**
+ * GHOST_LOG_CONTEXT_ENTER() - Enter a shadow stack context
+ *
+ * Puts this function on the shadow stack,
+ * must be paired with a sibling GHOST_LOG_CONTEXT_EXIT.
+ */
 #define GHOST_LOG_CONTEXT_ENTER() ghost_log_enter_context(__func__)
+
+/**
+ * GHOST_LOG_CONTEXT_EXIT() - Leave the current function's shadow stack context.
+ *
+ * Will fail if not in the same function as the current context.
+ */
+#define GHOST_LOG_CONTEXT_EXIT() ghost_log_exit_context(__func__)
+
+/**
+ * GHOST_LOG_CONTEXT_EXIT_FORCE() - Leave a shadow stack context.
+ * @CONTEXT_NAME: name of context.
+ *
+ * Leaves a shadow stack context
+ */
+#define GHOST_LOG_CONTEXT_EXIT_FORCE(CONTEXT_NAME) ghost_log_exit_context(CONTEXT_NAME)
+
+// Versions for inner contexts, too.
 #define GHOST_LOG_CONTEXT_ENTER_INNER(name) ghost_log_enter_context(__INNER_NAME(name))
 #define GHOST_LOG_CONTEXT_EXIT_INNER(name) ghost_log_exit_context(__INNER_NAME(name))
-#define GHOST_LOG_CONTEXT_EXIT() ghost_log_exit_context(__func__)
-#define GHOST_LOG_CONTEXT_EXIT_FORCE(s) ghost_log_exit_context(s)
 
 #define GHOST_LOG_P(context, var, printer) \
 	ghost_log_context_attach(context, #var, (void*)&(var), printer)
@@ -52,18 +76,51 @@ int gp_put_current_context_trace(gp_stream_t *out);
 #define GHOST_u32printer hyp_putx32ptr
 #define GHOST_boolprinter hyp_putboolptr
 #define GHOST_strprinter hyp_putsptr
+#define __GHOST_PRINTER(ty) GHOST_##ty##printer
 
-#define GHOST_PRINTER(ty) \
-	GHOST_##ty##printer
+/**
+ * GHOST_LOG() - Register a variable for logging.
+ *
+ * @VAR: lvalue
+ * @TY: known ghost type (u64, u32, bool, str).
+ *
+ * Puts a reference to VAR on the shadow stack,
+ * to be printed on any shadow traceback.
+ *
+ * Must be called from inside a same-function GHOST_LOG_CONTEXT_ENTER[_INNER] context.
+ */
+#define GHOST_LOG(VAR, TY) \
+	GHOST_LOG_P((__func__), (VAR), __GHOST_PRINTER(TY))
 
-#define GHOST_LOG(var, ty) \
-	GHOST_LOG_P((__func__), (var), GHOST_PRINTER(ty))
+/**
+ * GHOST_LOG_FORCE() - GHOST_LOG() without the checking.
+ * @CONTEXT: explicit name of context.
+ * @VAR: lvalue.
+ * @TY: ghost type.
+ *
+ * Like GHOST_LOG but can explicitly pass name of context.
+ */
+#define GHOST_LOG_FORCE(CONTEXT, VAR, TY) \
+	GHOST_LOG_P((CONTEXT), (VAR), __GHOST_PRINTER(TY))
 
-#define GHOST_LOG_FORCE(context, var, ty) \
-	GHOST_LOG_P((context), (var), GHOST_PRINTER(ty))
+/**
+ * GHOST_LOG_INNER() - GHOST_LOG() for inner contexts.
+ * @NAME: explicit name of inner context.
+ * @VAR: lvalue.
+ * @TY: ghost type.
+ *
+ * Like GHOST_LOG but for inner contexts.
+ */
+#define GHOST_LOG_INNER(NAME, VAR, TY) \
+	GHOST_LOG_P(__INNER_NAME(NAME), (VAR), __GHOST_PRINTER(TY))
 
-#define GHOST_LOG_INNER(name, var, ty) \
-	GHOST_LOG_P(__INNER_NAME(name), (var), GHOST_PRINTER(ty))
+/**
+ * GHOST_ERROR_VAR() - Like GHOST_LOG() but for errors only.
+ *
+ * No checking on context frame.
+ */
+#define GHOST_ERROR_VAR(var, ty) \
+	GHOST_LOG_P(NULL, (var), __GHOST_PRINTER(ty))
 
 #define GHOST_WARN(msg) \
 	ghost_log_context_log(msg, GHOST_LOG_ERROR)
