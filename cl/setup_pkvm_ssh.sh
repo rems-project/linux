1e2472ec1823acf0f8e54f255d8f556ab2742ded
Ben Simner <benjsimner@gmail.com>
2023-11-29 17:30:46 +0000

    (ghost/cl) add pre-built kvmtest binary

--- cl/setup_pkvm_ssh.sh
+++ cl/setup_pkvm_ssh.sh
@@ -2,11 +2,13 @@ set -e
 
 read -p "login: " PKVM_USER
 
-# copy in lkvm
+# copy in lkvm and unwrap it
 scp -P 8022 cl/lkvm/lkvm-static.zst ${PKVM_USER}@localhost:/home/${PKVM_USER}/
-
-# unwrap it
 ssh ${PKVM_USER}@localhost -p8022 'zstd --decompress lkvm-static.zst && chmod +x ./lkvm-static'
 
 # copy in a kernel Image
 scp -P 8022 arch/arm64/boot/Image ${PKVM_USER}@localhost:/home/${PKVM_USER}/
+
+# copy in kvmtest and unwrap it
+scp -P 8022 cl/kvmtest/kvmtest.zst ${PKVM_USER}@localhost:/home/${PKVM_USER}/
+ssh ${PKVM_USER}@localhost -p8022 'zstd --decompress kvmtest.zst && chmod +x ./kvmtest'
