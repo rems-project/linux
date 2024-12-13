3f603a1bce6f33019dbf39bbcf7c5b38e3a1cfbf
cp526 <cp526@cam.ac.uk>
2024-12-13 17:14:29 +0000

    inline unneeded CN definition file

--- arch/arm64/kvm/hyp/pgtable.c
+++ arch/arm64/kvm/hyp/pgtable.c
@@ -93,7 +93,51 @@ function (boolean) possible_mm_ops_agree (possible_mm_ops o, pointer m)
 @*/
 
 
-#include <asm/page_table_definitions.cn.h>
+// --- page table definitions --------------------------------------------------
+
+// We assume a setup where pages, and hence individual page tables,
+// are 4096 bytes in size (2^12). On a 64-bit platform (with pointers
+// of 2^3 bytes size) that means an individual page table can fit
+// 2^9=512 entries. An overall page table has maximum depth of four
+// levels (levels 0 -- 3). For any given 64-bit pointer, to be
+// address-translated, only a portion of up to 48 bits are the actual
+// address (the *input address*). In the most basic setup, these 48
+// bits are divided into four times 9 bits, which are taken as indices
+// into page table levels 0 through 3, with the remaining 12 bits used
+// as an offset into the page of physical memory obtained from the
+// page table lookup (if the address is mapped).
+// 
+// Different configurations from this basic setup are possible: input
+// addresses can be made smaller by setting an appropriate hardware
+// register, and the page table depth can be reduced by setting the
+// initial translation level to be greater than 0.
+//
+// For small input address sizes (relative to the number of
+// translation levels), the default table encoding would lead to
+// wasted space and pointer indirection, due to mostly-empty top level
+// page tables (i.e. 12 bits, plus 9 bits times the number of
+// translation levels, could encode significantly larger addresses
+// than the chosen input address size). 
+// 
+// When the input address size is sufficiently small for that, it is
+// then possible to optimise and use *concatenated page tables*:
+// instead of a regular level-n page-table setup, using the
+// concatenated-page-table scheme, one gets a level-(n-1) page table
+// where the top level becomes *an array of page tables* that
+// effectively represents the two upper levels of the page table
+// within a single page: the part of the input address dedicated for
+// the initial level of the address translation is used to index the
+// page-table array, the resulting page-table is indexed using the
+// next 9-bit index. When the input address size permits, the
+// concatenated scheme is selected by chosing an initial translation
+// level that is one greater than what one would have chosen
+// otherwise.
+//
+// Note: Armv8.2 introduced an architecture extension that allows for
+// input addresses of larger sizes, up to 52 bits, and initial address
+// translation levels of -1, which our specification does not handle
+// yet.
+
 
 
 /*@ 
@@ -136,29 +180,9 @@ function (boolean) cn_pte_table (kvm_pte_t pte, u32 level)
 }
 
 
-// more abstract CN counterparts to table queries
 // - note about level: in this ARM pgtable, the page & table encodings are
 //   shared, and entries at the final level are automatically not tables 
 
-// function (boolean) is_possible_table_entry1 (u64 return)
-// { 
-//   kvm_pte_table(return, 0u32) == 1u8 
-// }
-
-// function (boolean) is_possible_table_entry (u64 return)
-// { 
-//   cn_pte_valid(return) && is_possible_table_entry1(return) 
-// }
-
-// function (boolean) level_has_tables (u32 level)
-// { 
-//   valid_pgtable_level(level + 1u32) 
-// }
-
-// function (boolean) is_table_entry_at (u64 encoded, u32 level)
-// { 
-//   is_possible_table_entry(encoded) && level_has_tables(level) 
-// }
 
 function (pointer) decode_table_entry_pointer (u64 encoded)
 {
@@ -1862,7 +1886,6 @@ void kvm_pgtable_stage2_free_removed(struct kvm_pgtable_mm_ops *mm_ops, void *pg
 /* more verification hacks */
 int verification_deps (void) {
   (void) include_allocator_spec;
-  (void) include_page_table_definitions;
   (void) hyp_zalloc_hyp_page;
   (void) hyp_phys_to_virt;
   (void) hyp_virt_to_phys;
