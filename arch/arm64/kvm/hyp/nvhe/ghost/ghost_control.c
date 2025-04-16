339d18f40b3be9926684c756fa5318031130c9da
Ben Simner <benjsimner@gmail.com>
2025-04-16 15:04:52 +0000

    (ghost/Kconfig) restructure Casemate menu options
    From pkvm-verif-6.4: 44545f330c9571a4cf6a4f10f7114c20fd1ae9ca

--- arch/arm64/kvm/hyp/nvhe/ghost/ghost_control.c
+++ arch/arm64/kvm/hyp/nvhe/ghost/ghost_control.c
@@ -9,9 +9,9 @@ static const bool verbose_spec = IS_ENABLED(CONFIG_NVHE_GHOST_SPEC_VERBOSE);
 
 static const bool check_sm = (
 	   IS_ENABLED(CONFIG_NVHE_GHOST_SIMPLIFIED_MODEL)
-	&& !IS_ENABLED(CONFIG_NVHE_GHOST_SIMPLIFIED_MODEL_LOG_ONLY)
+	&& IS_ENABLED(CONFIG_NVHE_GHOST_CASEMATE_CHECK)
 );
-static const bool noisy_sm = IS_ENABLED(CONFIG_NVHE_GHOST_SIMPLIFIED_MODEL_NOISY);
+static const bool log_sm = IS_ENABLED(CONFIG_NVHE_GHOST_CASEMATE_LOG);
 
 struct ghost_control_item {
 	const char* name;
@@ -31,8 +31,8 @@ static struct ghost_control_item ghost_controls[] = {
 	(struct ghost_control_item){.name="setup", .check=true, .print=noisy_spec},
 
 	// printing simplified model (#define disables checking entirely);
-	(struct ghost_control_item){.name="casemate_model_step", .check=check_sm, .print=noisy_sm},
-	(struct ghost_control_item){.name="initialise_ghost_simplified_model", .check=true, .print=noisy_sm},
+	(struct ghost_control_item){.name="casemate_model_step", .check=check_sm, .print=log_sm},
+	(struct ghost_control_item){.name="initialise_ghost_simplified_model", .check=check_sm, .print=log_sm},
 	(struct ghost_control_item){.name="sm_dump_trans", .check=true, .print=IS_ENABLED(CONFIG_NVHE_GHOST_SIMPLIFIED_MODEL_DUMP_ON_TRANS)},
 	(struct ghost_control_item){.name="sm_diff_trans", .check=true, .print=IS_ENABLED(CONFIG_NVHE_GHOST_SIMPLIFIED_MODEL_DIFF_ON_TRANS)},
 	(struct ghost_control_item){.name="sm_condensed", .check=true, .print=IS_ENABLED(CONFIG_NVHE_GHOST_SIMPLIFIED_MODEL_NOISY_CONDENSED)},
